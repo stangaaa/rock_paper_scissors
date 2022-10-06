c79cb6f3d7b85d5c7461bd56ec8d780d3f363c6d
stangaaa <giostanga@gmail.com>
2022-10-06 23:46:24 +0200

    drastic changes in javascript code, implemted buttons to make player choice this code is complete garbage but it works just fine

--- script.js
+++ script.js
@@ -1,46 +1,75 @@
-let playerWins = 0;
-let cpuWins = 0;
-let draws = 0;
+const rock = document.getElementById('btn_1');
+const paper = document.getElementById('btn_2');
+const scissors = document.getElementById('btn_3');
 
-function getComputerchioce() {
-    computerChoice = Math.floor(Math.random() * 3) + 1;
-
-    if (computerChoice === 1) return "rock";
-    else if (computerChoice === 2) return "paper";
-    else return "scissors";
-    
+let choice = 0;
+let computer_wins = 0;
+let player_wins = 0;
+function computerChoice() { 
+    return Math.floor(Math.random() * (2 - 0 + 1) + 0)
+    //0 is rock
+    //1 is paper
+    //2 is scissors
 }
 
-function playerChoice() {
-    let choice =  prompt("");
-    return choice.toLowerCase()
-}
-
-function playRound(playerChoice, computerChoice){
-
-    if(playerChoice === computerChoice) {
-        draws++;
-        return console.log("draw")
+function score_check(){
+    if(player_wins === 5){
+        console.log("THE PLAYER WON")
+        player_wins = 0;
+        computer_wins =0;
     }
-    else if ((playerChoice === "paper" && computerChoice === "rock") || ( playerChoice === "scissors" && computerChoice === "paper") || (playerChoice === "rock" && computerChoice === "scissors")){
-        playerWins++;
-        return console.log("player won")
-
-    }
-    else if ((playerChoice === "paper" && computerChoice === "scissors") || ( playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "scissors" && computerChoice === "rock")){
-        cpuWins++;
-        return console.log("cpu won")
-    }
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerChoice(), getComputerchioce())
+    if(computer_wins === 5){
+        console.log("THE CPU WON")
+        player_wins = 0;
+        computer_wins =0;
     }
 }
 
 
-console.log(game())
-console.log( "player won " + playerWins)
-console.log( "cpu won " + cpuWins)
-console.log( "draws " + draws)
+rock.addEventListener("click", () => {
+    choice = 0;
+    if(choice === computerChoice()){
+    console.log("draw")
+    }
+    else if (computerChoice() === 1){
+        console.log("computer won");
+        computer_wins = computer_wins + 1
+    }
+    else{
+        console.log("player won");
+        player_wins = player_wins + 1;
+    }
+    score_check()
+    ;
+
+})
+
+paper.addEventListener("click", () => {
+    choice = 1;
+    if(choice === computerChoice()) console.log("draw")
+    else if (computerChoice() === 2){
+        console.log("computer won")
+        computer_wins = computer_wins + 1
+        console.log(computer_wins)
+
+    }
+    else{
+        console.log("player won");
+        player_wins = player_wins + 1;
+    }
+    score_check()
+    })
+
+scissors.addEventListener("click", () => {
+    choice = 2;
+    if(choice === computerChoice()) console.log("draw")
+    else if (computerChoice() === 0){
+        console.log("computer won");
+        computer_wins = computer_wins + 1
+    }
+    else{
+        console.log("player won");
+        player_wins = player_wins + 1;
+    }
+    score_check()
+})
