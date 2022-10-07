3587d2d65593006659e0b352725324880d70440e
stangaaa <giostanga@gmail.com>
2022-10-07 15:37:41 +0200

    implemented score counters in html

--- script.js
+++ script.js
@@ -2,27 +2,37 @@ const rock = document.getElementById('btn_1');
 const paper = document.getElementById('btn_2');
 const scissors = document.getElementById('btn_3');
 
+const counter_1 = document.getElementById('player-wins')
+const counter_2 = document.getElementById('computer-wins')
+const counter_3 = document.getElementById('winner')
+
 let choice = 0;
 let computer_wins = 0;
 let player_wins = 0;
+
+counter_1.innerText = 'player wins: ' + player_wins;
+counter_2.innerText = 'cpu wins: ' + computer_wins;
+
+function score_counter(){
+    counter_1.innerText = 'player wins: ' + player_wins;
+    counter_2.innerText = 'cpu wins: ' + computer_wins;
+    if(player_wins === 5){
+        counter_3.innerText = 'THE PLAYER WON!'
+        player_wins = 0;
+        computer_wins = 0;
+    }
+    else if(computer_wins === 5){
+        counter_3.innerText = 'THE CPU WON!'
+        player_wins = 0;
+        computer_wins = 0;
+    }
+}
+
 function computerChoice() { 
     return Math.floor(Math.random() * (2 - 0 + 1) + 0)
     //0 is rock
     //1 is paper
     //2 is scissors
-}
-
-function score_check(){
-    if(player_wins === 5){
-        console.log("THE PLAYER WON")
-        player_wins = 0;
-        computer_wins =0;
-    }
-    if(computer_wins === 5){
-        console.log("THE CPU WON")
-        player_wins = 0;
-        computer_wins =0;
-    }
 }
 
 
@@ -39,8 +49,7 @@ rock.addEventListener("click", () => {
         console.log("player won");
         player_wins = player_wins + 1;
     }
-    score_check();
-
+    score_counter();
 })
 
 paper.addEventListener("click", () => {
@@ -56,7 +65,7 @@ paper.addEventListener("click", () => {
         console.log("player won");
         player_wins = player_wins + 1;
     }
-    score_check()
+    score_counter();
     })
 
 scissors.addEventListener("click", () => {
@@ -70,5 +79,5 @@ scissors.addEventListener("click", () => {
         console.log("player won");
         player_wins = player_wins + 1;
     }
-    score_check()
+    score_counter();
 })
